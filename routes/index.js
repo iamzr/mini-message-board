ec5f78ccbc98ae8f3d9d9d8604c1ecf3ec77cc2b
Zia Rehman <iamziarehman@gmail.com>
2021-09-30 18:41:07 +0100

    Added messageboard and new form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hey guys!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET new form page */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+/* POST data from new form page */
+router.post("/new", function (req, res, next) {
+  const text = req.body.text;
+  const user = req.body.user;
+
+  messages.push({ text: text, user: user, added: new Date() });
+
+  res.redirect("/");
 });
 
 module.exports = router;
